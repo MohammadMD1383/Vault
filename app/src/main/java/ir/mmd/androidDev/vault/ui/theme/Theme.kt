fc53917f297c9c02caaac7a4804649704418c96f
Mohammad Mostafa <ezmmd83@gmail.com>
2023-09-25 20:17:46 +0330

    fixed status bar color

--- app/src/main/java/ir/mmd/androidDev/vault/ui/theme/Theme.kt
+++ app/src/main/java/ir/mmd/androidDev/vault/ui/theme/Theme.kt
@@ -38,8 +38,8 @@ fun VaultTheme(
 	if (!view.isInEditMode) {
 		SideEffect {
 			val window = (view.context as Activity).window
-			window.statusBarColor = colorScheme.primary.toArgb()
-			WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+			window.statusBarColor = colorScheme.surface.toArgb()
+			WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
 		}
 	}
 	
