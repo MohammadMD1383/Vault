029a1ab94e570ccb35627f20ec278646594db1e3
Mohammad Mostafa <ezmmd83@gmail.com>
2023-09-25 21:46:01 +0330

    updated to v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 		applicationId = "ir.mmd.androidDev.vault"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 2
-		versionName = "1.1.0"
+		versionCode = 3
+		versionName = "1.2.0"
 		
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
