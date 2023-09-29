0462f056979435ebeafde9b8dd28d1eab73fe367
Mohammad Mostafa <ezmmd83@gmail.com>
2023-09-29 15:13:05 +0330

    updated build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
@@ -25,6 +27,7 @@ android {
 			isMinifyEnabled = true
 			isShrinkResources = true
 			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+			archivesName.set("Vault-${defaultConfig.versionName}")
 		}
 	}
 	
@@ -56,11 +59,11 @@ dependencies {
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-alpha02")
 	implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-alpha02")
-	implementation("androidx.compose.runtime:runtime-livedata:1.5.1")
+	implementation("androidx.compose.runtime:runtime-livedata:1.5.2")
 	implementation("androidx.activity:activity-compose:1.7.2")
 	implementation("androidx.core:core-splashscreen:1.0.1")
 	implementation("androidx.biometric:biometric:1.1.0")
-	implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+	implementation(platform("androidx.compose:compose-bom:2023.09.02"))
 	implementation("androidx.navigation:navigation-compose:2.7.3")
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
@@ -71,7 +74,7 @@ dependencies {
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.02"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
