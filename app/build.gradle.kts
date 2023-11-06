f71cf53196ae86759921faa90119c816d3ef85d5
Mohammad Mostafa <ezmmd83@gmail.com>
2023-11-06 20:15:27 +0330

    updated some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 }
 
 dependencies {
-	implementation("androidx.fragment:fragment-ktx:1.6.1")
+	implementation("androidx.fragment:fragment-ktx:1.6.2")
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-alpha03")
 	implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-alpha03")
@@ -65,7 +65,7 @@ dependencies {
 	implementation("androidx.core:core-splashscreen:1.0.1")
 	implementation("androidx.biometric:biometric:1.1.0")
 	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-	implementation("androidx.navigation:navigation-compose:2.7.4")
+	implementation("androidx.navigation:navigation-compose:2.7.5")
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
